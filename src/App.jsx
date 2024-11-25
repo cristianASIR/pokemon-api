e481ca3d5c1b833d53bef2382951dbc9a5ba7e77
cristianASIR <ccorfer1005@g.educaand.es>
2024-11-25 22:53:50 +0100

    modf

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Index from './rutas/Index'
 import Add from './rutas/Add'
+import Navbar from './componentes/Nav'
 function App() {
   return (
     <>
+    <div>
+      <Navbar/>
+    </div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Index />} />
